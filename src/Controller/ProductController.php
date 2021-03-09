66a47f79fa3eac96eeee4dcf823ea7dd0995c454
Krikod <kristen.kod@gmail.com>
2021-03-09 19:12:17 +0100

    urlGenerator and path() -> dynamic url

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ProductController extends AbstractController
 {

69095d0a44e1b0e302ed8129e994065f0abd03fa
Krikod <kristen.kod@gmail.com>
2021-03-09 09:57:02 +0100

    Create page of a product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CategoryRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -26,5 +27,23 @@ class ProductController extends AbstractController
         return $this->render('product/category.html.twig', [
 			'category' => $category
         ]);
+    }
+
+	/**
+	 * @Route("/{category_slug}/{slug}", name="product_show")
+	 */
+	public function show($slug, ProductRepository $product_repository): Response
+	{
+		$product = $product_repository->findOneBy([
+			'slug' => $slug
+		]);
+
+		if (!$product) {
+			throw $this->createNotFoundException("Le produit demandé n'existe pas.");
+		}
+
+		return $this->render('product/show.html.twig', [
+			'product' => $product
+		]);
     }
 }

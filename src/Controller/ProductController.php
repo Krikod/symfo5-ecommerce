f5cb02de38683066e6987972efb7714b73607286
Krikod <kristen.kod@gmail.com>
2021-03-12 20:10:35 +0100

    Use FormFactoryInterface to create form for product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,13 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ProductController extends AbstractController
 {
@@ -45,6 +50,54 @@ class ProductController extends AbstractController
 
 		return $this->render('product/show.html.twig', [
 			'product' => $product
+		]);
+    }
+
+	/**
+	 * @Route("/admin/product/create", name="product_create")
+	 */
+	public function create(FormFactoryInterface $factory) {
+		$builder = $factory->createBuilder();
+		$builder
+			->add('name', TextType::class, [
+			'label' => 'Nom du produit',
+			'attr' => [
+				'class' => 'form-control',
+				'placeholder' => 'Tapez le nom du produit'
+			]
+		])
+			->add('shortDescription', TextareaType::class, [
+				'label' => 'Description courte',
+				'attr' => [
+					'class' => 'form-control',
+					'placeholder' => 'Tapez une description assez courte mais parlante pour le visiteur'
+				]
+			])
+		->add('price', MoneyType::class, [
+			'label' => 'Prix du produit',
+			'attr' => [
+				'class' => 'form-control',
+				'placeholder' => 'Tapez le prix du produit en Euros'
+			]
+		])
+		->add('category', EntityType::class, [
+			'label' => 'Catégorie',
+			'attr' => ['class' => 'form-control'],
+//			Pas le placeholder des attributs html de la liste, mais OPTION du champ ChoiceType !
+			'placeholder' => '-- Choisir une catégorie --',
+			'class' => Category::class,
+//			Choice_label ==> Fonction ou Nom de la category: 'name'
+			'choice_label' => function(Category $category) {
+				return strtoupper($category->getName());
+			}
+		]);
+
+		$form = $builder->getForm();
+
+		$formView = $form->createView();
+
+		return $this->render('product/create.html.twig', [
+			'formView' => $formView
 		]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,9 +4,12 @@ namespace App\DataFixtures;
 
 use App\Entity\Category;
 use App\Entity\Product;
+use Bezhanov\Faker\Provider\Commerce;
+use Bluemmb\Faker\PicsumPhotosProvider;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Liior\Faker\Prices;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
@@ -20,9 +23,9 @@ class AppFixtures extends Fixture
 	public function load(ObjectManager $manager)
     {
     	$faker = Factory::create('fr_FR');
-    	$faker->addProvider(new \Liior\Faker\Prices($faker));
-		$faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
-		$faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
+    	$faker->addProvider(new Prices($faker));
+		$faker->addProvider(new Commerce($faker));
+		$faker->addProvider(new PicsumPhotosProvider($faker));
 
 		for ($c = 0; $c < 3; $c++) {
 			$category = new Category();
@@ -35,10 +38,12 @@ class AppFixtures extends Fixture
 				$product = new Product();
 				$product->setName($faker->productName) // Bezhanov
 				        ->setPrice($faker->price(4000, 20000)) // lib Liior
-				        ->setSlug(strtolower($this->slugger->slug($product->getName()))) // construct: fait slug à partir du nom du produit
+						// construct: slug à partir du nom du produit
+						->setSlug(strtolower($this->slugger->slug($product->getName())))
 						->setCategory($category)
 						->setShortDescription($faker->paragraph())
-						->setMainPicture($faker->imageUrl(400, 400, true)); // bluemmb, true=images différentes
+						// bluemmb, true=images différentes
+						->setMainPicture($faker->imageUrl(400, 400, true));
 
 				$manager->persist($product);
 			}

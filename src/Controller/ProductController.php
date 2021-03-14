c0669af611ec8616207e55753545f67838e7315d
Krikod <kristen.kod@gmail.com>
2021-03-14 16:09:33 +0100

    Controller persist & flush Product to db

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,17 +3,22 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Entity\Product;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProductController extends AbstractController
 {
@@ -56,8 +61,16 @@ class ProductController extends AbstractController
 	/**
 	 * @Route("/admin/product/create", name="product_create")
 	 */
-	public function create(FormFactoryInterface $factory) {
-		$builder = $factory->createBuilder();
+	public function create(
+		FormFactoryInterface $factory,
+		Request $request,
+		SluggerInterface $slugger,
+		EntityManagerInterface $em) {
+
+		$builder = $factory->createBuilder(FormType::class, null, [
+			'data_class' => Product::class
+		]);
+
 		$builder
 			->add('name', TextType::class, [
 			'label' => 'Nom du produit',
@@ -77,6 +90,10 @@ class ProductController extends AbstractController
 				'placeholder' => 'Tapez le prix du produit en Euros'
 			]
 		])
+			->add('mainPicture', UrlType::class, [
+				'label' => 'Image du produit',
+				'attr' => ['placeholder' => 'Tapez l\'URL de l\'image']
+			])
 		->add('category', EntityType::class, [
 			'label' => 'Catégorie',
 //			Pas le placeholder des attributs html de la liste, mais OPTION du champ ChoiceType !
@@ -90,7 +107,22 @@ class ProductController extends AbstractController
 
 		$form = $builder->getForm();
 
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted()) {
+			$product = $form->getData();
+			$product->setSlug(strtolower($slugger->slug($product->getName())));
+
+		$em->persist($product);
+		$em->flush();
+			dump($product);
+		}
+
+
+
 		$formView = $form->createView();
+
+
 
 		return $this->render('product/create.html.twig', [
 			'formView' => $formView

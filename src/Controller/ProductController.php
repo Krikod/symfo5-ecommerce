ec2b35d1f4b66df28385f0108092f67d6d1d66d1
Krikod <kristen.kod@gmail.com>
2021-03-15 19:04:35 +0100

    create & edit a category

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -11,7 +11,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ProductController extends AbstractController
@@ -57,7 +56,7 @@ class ProductController extends AbstractController
 	 */
 	public function edit($id, ProductRepository $repo,
 		Request $request, EntityManagerInterface $em,
-		SluggerInterface $slugger, UrlGeneratorInterface $url_generator) {
+		SluggerInterface $slugger) {
 
 		$product = $repo->find($id);
 

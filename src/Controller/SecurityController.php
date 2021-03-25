d0c66bb6d7177a671614aca47565cd4a94389f63
Krikod <kristen.kod@gmail.com>
2021-03-25 22:21:46 +0100

    Manual authentication with LoginFormAuthenticator, then Sf form_login

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\LoginType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="security_login")
+     */
+    public function login(AuthenticationUtils $utils): Response
+    {
+    	$form = $this->createForm( LoginType::class, ['email' => $utils->getLastUsername()]);
+
+        return $this->render('security/login.html.twig', [
+        	'formView' => $form->createView(),
+	        'error' => $utils->getLastAuthenticationError(),
+        ]);
+    }
+}
---
+++ src/Security/LoginFormAuthenticator.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class LoginFormAuthenticator extends AbstractGuardAuthenticator
+{
+	protected $encoder;
+
+	public function __construct( UserPasswordEncoderInterface $encoder ) {
+		$this->encoder = $encoder;
+	}
+	
+    public function supports(Request $request)
+    {
+    	return $request->attributes->get( '_route') === 'security_login'
+		    && $request->isMethod( 'POST');
+    }
+
+    public function getCredentials(Request $request)
+    {
+	    return $request->request->get('login'); // array 3 infos
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+    	try {
+		    return $userProvider->loadUserByUsername( $credentials['email']);
+	    } catch (UsernameNotFoundException $e) {
+    		throw new AuthenticationException("Cette adresse email n'est pas connue");
+	    }
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+//    	return $this->encoder->isPasswordValid( $user, $credentials['password']);
+    	$isValid = $this->encoder->isPasswordValid( $user, $credentials['password']);
+
+    	if (!$isValid) {
+    		throw new AuthenticationException("Les informations de connexion ne correspondent pas");
+	    }
+	    return true;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+		$request->attributes->set( Security::AUTHENTICATION_ERROR, $exception);
+	    $login = $request->request->get('login');
+	    $request->attributes->set(Security::LAST_USERNAME, $login['email']);
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
+    {
+        return new RedirectResponse( '/');
+    }
+
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        // todo
+    }
+
+    public function supportsRememberMe()
+    {
+        // todo
+    }
+}

cca0a4987f58cbd745e9fe84f390dfb55093da2d
Krikod <kristen.kod@gmail.com>
2021-03-31 19:53:24 +0200

    SessionInterface & FlashBags & Twig

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -6,6 +6,8 @@ use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
@@ -13,7 +15,7 @@ class CartController extends AbstractController
     /**
      * @Route("/cart/add/{id}", name="cart_add", requirements={"id":"\d+"})
      */
-    public function add($id, Request $request, ProductRepository $repo): Response
+    public function add($id, ProductRepository $repo, SessionInterface $session): Response
     {
     	// Produit existe ?
 	    $product = $repo->find( $id);
@@ -22,7 +24,8 @@ class CartController extends AbstractController
 	    }
     	// Retrouver le panier dans la session : []
 	    // S'il n'existe pas, prendre un [] vide
-		$cart = $request->getSession()->get( 'cart', []);
+//		$cart = $request->getSession()->get( 'cart', []);
+		$cart = $session->get( 'cart', []);
 
 		// Voir si le produit $id existe dans le tab: [12 => 3, 29 => 2]
 	    // Si oui, augmenter la quantité / Sinon, ajouter pduit avec q 1
@@ -33,8 +36,20 @@ class CartController extends AbstractController
 	    }
 
 	    // Enregistrer le tab mis à jour dans la session
-	    $request->getSession()->set( 'cart', $cart);
+//	    $request->getSession()->set( 'cart', $cart);
+	    $session->set( 'cart', $cart);
 //		$request->getSession()->remove( 'cart');
+
+//	    dd( $session->get('cart'));
+//	    dd( $session->getBag( 'flashes'));
+
+	    /** @var FlashBag */
+	    $flashBag = $session->getBag( 'flashes');
+
+		$flashBag->add('success', "Le produit a bien été ajouté au panier");
+//		$flashBag->add('warning', "Attention !");
+//		dump( $flashBag->get('success'));
+//	    dd($flashBag);
 
 	    return $this->redirectToRoute( 'product_show', [
 	    	'category_slug' => $product->getCategory()->getSlug(),

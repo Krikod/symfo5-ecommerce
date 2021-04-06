25a89111f967ebff5f9fbd805ad06e41c8178964
Krikod <kristen.kod@gmail.com>
2021-04-06 12:48:30 +0200

    Purchase Entity, relation/User, fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Category;
 use App\Entity\Product;
+use App\Entity\Purchase;
 use App\Entity\User;
 use Bezhanov\Faker\Provider\Commerce;
 use Bluemmb\Faker\PicsumPhotosProvider;
@@ -43,13 +44,17 @@ class AppFixtures extends Fixture
 			->setRoles( ['ROLE_ADMIN']);
 
 		$manager->persist( $admin);
-		
+
+		$users = [];
+
 		for ($u = 0; $u < 5; $u++) {
 			$user = new User();
 			$hash = $this->encoder->encodePassword( $user, "password");
 			$user->setEmail( "user-$u@gmail.com")
 			     ->setFullName( $faker->name())
 				->setPassword( $hash);
+
+			$users[] = $user;
 
 			$manager->persist( $user);
 		}
@@ -76,6 +81,23 @@ class AppFixtures extends Fixture
 				$manager->persist($product);
 			}
         }
+
+        // Purchase
+		for ($p = 0; $p < mt_rand(20, 40); $p++) {
+			$purchase = new Purchase();
+			$purchase->setFullName( $faker->name)
+				->setAddress( $faker->streetAddress)
+				->setPostalCode( $faker->postcode)
+				->setCity( $faker->city)
+				->setUser( $faker->randomElement($users))
+				->setTotal( mt_rand(2000, 30000));
+
+			if ($faker->boolean(90)) {
+				$purchase->setStatus(Purchase::STATUS_PAID);
+			}
+
+			$manager->persist( $purchase);
+		}
 
         $manager->flush();
     }

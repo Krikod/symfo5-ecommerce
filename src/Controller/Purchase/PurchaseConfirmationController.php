81858da5536e4c88310d1af27a14803e18f28501
Krikod <kristen.kod@gmail.com>
2021-04-14 10:04:29 +0200

    Refactoring PurchaseConfirmationController

--- src/Controller/Purchase/PurchaseConfirmationController.php
+++ src/Controller/Purchase/PurchaseConfirmationController.php
@@ -7,7 +7,7 @@ use App\Entity\Purchase;
 use App\Entity\PurchaseItem;
 use App\Form\CartConfirmationType;
 use Doctrine\ORM\EntityManagerInterface;
-use Faker\Provider\DateTime;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -21,48 +21,58 @@ use Symfony\Component\Security\Core\Security;
 
 class PurchaseConfirmationController extends AbstractController {
 
-	protected $factory;
-	protected $router;
-	protected $security;
+//	protected $factory;
+//	protected $router;
+//	protected $security;
 	protected $cart;
 	protected $em;
 
-	public function __construct(FormFactoryInterface $factory, RouterInterface $router,
-		Security $security, CartService $cart, EntityManagerInterface $em) {
-		$this->factory = $factory;
-		$this->router = $router;
-		$this->security = $security;
+	public function __construct(CartService $cart, EntityManagerInterface $em) {
+//		$this->factory = $factory;
+//		$this->router = $router;
+//		$this->security = $security;
 		$this->cart = $cart;
 		$this->em = $em;
 	}
 
 	/**
 	 * @Route("purchase/confirm", name="purchase_confirm")
+	 * @IsGranted("ROLE_USER", message="Vous devez être connecté pour confirmer une commande")
 	 */
-	public function confirm( Request $request, FlashBagInterface $flash_bag): Response {
+	public function confirm( Request $request): Response {
 		//** 1. Lire données formulaire -> FormFactoryInterface, Request **//
-		$form = $this->factory->create( CartConfirmationType::class);
+//		$form = $this->factory->create( CartConfirmationType::class);
+		$form = $this->createForm( CartConfirmationType::class);
+
 		$form->handleRequest($request);
+
 
 		//** 2. Si formulaire non soumis: dégager ! **//
 		if (!$form->isSubmitted()) {
 			// Message flash puis redirection -> SessionInterface ? Mieux, FlashBagInterface
-			$flash_bag->add( 'warning', 'Vous devez remplir le formulaire de confirmation');
-			return new RedirectResponse( $this->router->generate('cart_show'));
+//			$flash_bag->add( 'warning', 'Vous devez remplir le formulaire de confirmation');
+//			return new RedirectResponse( $this->router->generate('cart_show'));
+			$this->addFlash( 'warning', 'Vous devez remplir le formulaire de confirmation');
+			return $this->redirectToRoute( 'cart_show');
 		}
 
+
 		//** 3. Si pas connecté: dégager ! -> Security **//
-		$user = $this->security->getUser();
-		if (!$user) {
-			throw new AccessDeniedException("Vous devez être connecté pour confirmer une commande");
-		}
+//		$user = $this->security->getUser();
+		$user = $this->getUser();
+
+//		if (!$user) {
+//			throw new AccessDeniedException("Vous devez être connecté pour confirmer une commande");
+//		}
 
 		// 4. Si form soumis et connecté MAIS si pas de produit dans panier: dégager !
 		// -> SessionInterface ou CartService
 		$cartItems = $this->cart->getDetailedCartItems();
 		if (count($cartItems) === 0) {
-			$flash_bag->add( 'warning', 'Vous ne pouvez pas confirmer une commande avec un panier vide');
-			return new RedirectResponse( $this->router->generate( 'cart_show'));
+//			$flash_bag->add( 'warning', 'Vous ne pouvez pas confirmer une commande avec un panier vide');
+//			return new RedirectResponse( $this->router->generate( 'cart_show'));
+			$this->addFlash( 'warning', 'Vous ne pouvez pas confirmer une commande avec un panier vide');
+			return $this->redirectToRoute( 'cart_show');
 		}
 
 		// 5. Si tout ok, créer une purchase
@@ -98,8 +108,9 @@ class PurchaseConfirmationController extends AbstractController {
 		// 8. Enregistrer la commande -> EntityManagerInterface
 		$this->em->flush();
 
-		$flash_bag->add( 'success', 'La commande a bien été enregistrée');
-		return new RedirectResponse( $this->router->generate( 'purchases_index'));
+//		$flash_bag->add( 'success', 'La commande a bien été enregistrée');
+//		return new RedirectResponse( $this->router->generate( 'purchases_index'));
+		$this->addFlash( 'success', 'La commande a bien été enregistrée');
+		return $this->redirectToRoute( 'purchases_index');
 	}
-
 }

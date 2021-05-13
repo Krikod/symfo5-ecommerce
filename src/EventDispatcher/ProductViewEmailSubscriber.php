c4924fc4fd73513fcd3ce016e87aa60e227d9db3
Krikod <kristen.kod@gmail.com>
2021-05-13 20:29:32 +0200

    Install Mailtrap mailer + send test email with TemplatedEmail

---
+++ src/EventDispatcher/ProductViewEmailSubscriber.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\EventDispatcher;
+
+
+use App\Event\ProductViewEvent;
+use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\Email;
+
+class ProductViewEmailSubscriber implements EventSubscriberInterface {
+	protected $logger;
+
+	protected $mailer;
+
+	public function __construct(LoggerInterface $logger, MailerInterface $mailer) {
+		$this->logger = $logger;
+		$this->mailer = $mailer;
+	}
+
+	public static function getSubscribedEvents() {
+		return [
+			'product.view' => 'sendEmail'
+		];
+	}
+
+	public function sendEmail(ProductViewEvent $event) {
+		
+		$email = new TemplatedEmail();
+		$email->from(new Address("contact@mail.com", "Infos de la boutique"))
+			->to("admin@mail.com")
+			->text("un visiteur est en train de voir la page du produit n° " .
+			$event->getProduct()->getId())
+//			->html("<h1>Visite du produit n° {$event->getProduct()->getId()}</h1>")
+				->htmlTemplate('emails/product_view.html.twig')
+			->context([
+				'product' => $event->getProduct()
+			])
+			->subject("Visite du produit n° " . $event->getProduct()->getId());
+
+		$this->mailer->send($email);
+		
+		$this->logger->info("Un email a été envoyé à l'admin pour le produit n° " .
+		                    $event->getProduct()->getId());
+	}
+}

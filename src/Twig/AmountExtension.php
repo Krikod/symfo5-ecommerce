66d541abb6a3e195d4678c471905947980e54c87
Krikod <kristen.kod@gmail.com>
2021-05-17 10:31:51 +0200

    Create a Twig filter: |amount

---
+++ src/Twig/AmountExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AmountExtension extends AbstractExtension {
+	public function getFilters(  ) {
+		return [
+			new TwigFilter('amount', [$this, 'amount'] )
+		];
+	}
+
+	public function amount($value) {
+//		dd( $value);
+		$finalValue = $value / 100;
+		$finalValue = number_format($finalValue, 2, ',', ' ');
+		// 192,29
+
+		return $finalValue . ' €';
+	}
+}

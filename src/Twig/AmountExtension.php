b4fdf3ddc8f4d165ffa7b168504587e13080be91
Krikod <kristen.kod@gmail.com>
2021-05-18 10:46:44 +0200

    Add params in custom Twig filter for prices

--- src/Twig/AmountExtension.php
+++ src/Twig/AmountExtension.php
@@ -6,18 +6,18 @@ use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
 class AmountExtension extends AbstractExtension {
-	public function getFilters(  ) {
+	public function getFilters(  ): array {
 		return [
 			new TwigFilter('amount', [$this, 'amount'] )
 		];
 	}
 
-	public function amount($value) {
+	public function amount($value, string $symbol = ' €', string $decsep = ',', string $thousandsep = ' ') {
 //		dd( $value);
 		$finalValue = $value / 100;
-		$finalValue = number_format($finalValue, 2, ',', ' ');
+		$finalValue = number_format($finalValue, 2, $decsep, $thousandsep);
 		// 192,29
 
-		return $finalValue . ' €';
+		return $finalValue . $symbol;
 	}
 }
